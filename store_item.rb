ea36ada8f549e118d45c142028cba80d6d9e4721
Marisha Sahay <marishasahay@gmail.com>
2016-07-11 20:11:11 -0700

    Fix as price in first has was string instead of integer

--- store_item.rb
+++ store_item.rb
@@ -1,4 +1,4 @@
 puts "This is my dog shop"
-dog1 = {:breed => "retriever",:color => "golden", :price => "500"}
+dog1 = {:breed => "retriever",:color => "golden", :price => 500}
 dog2 = {:breed => "bulldog", :color => "black", :price => 300}
 dog3 = {breed: "pug", color: "brown", price: 230}
